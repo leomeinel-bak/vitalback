bf4e414be09b6f391c84969eef5861b2d13bfd2b
TamrielNetwork <contact@tamriel.me>
2022-03-04 17:09:52 +0100

    Cancel if delay is active

--- src/main/java/com/tamrielnetwork/vitalback/utils/commands/CmdSpec.java
+++ src/main/java/com/tamrielnetwork/vitalback/utils/commands/CmdSpec.java
@@ -21,7 +21,6 @@ package com.tamrielnetwork.vitalback.utils.commands;
 import com.google.common.collect.ImmutableMap;
 import com.tamrielnetwork.vitalback.VitalBack;
 import com.tamrielnetwork.vitalback.utils.Chat;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -29,26 +28,39 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CmdSpec {
 
 	private static final VitalBack main = JavaPlugin.getPlugin(VitalBack.class);
+	private static final List<UUID> onActiveDelay = new ArrayList<>();
 
 	public static void doDelay(CommandSender sender, Location location) {
 
 		Player senderPlayer = (Player) sender;
 
 		if (!sender.hasPermission("vitalspawn.delay.bypass")) {
+			if (onActiveDelay.contains(senderPlayer.getUniqueId())) {
+				Chat.sendMessage(sender, "active-delay");
+				return;
+			}
+			onActiveDelay.add(senderPlayer.getUniqueId());
 			String timeRemaining = String.valueOf(main.getConfig().getLong("delay.time"));
 			Chat.sendMessage(sender, ImmutableMap.of("%countdown%", timeRemaining), "countdown");
 			new BukkitRunnable() {
 
 				@Override
 				public void run() {
+
 					if (Cmd.isInvalidPlayer(senderPlayer)) {
+						onActiveDelay.remove(senderPlayer.getUniqueId());
 						return;
 					}
 
 					senderPlayer.teleport(location);
+					onActiveDelay.remove(senderPlayer.getUniqueId());
 				}
 			}.runTaskLater(main, (main.getConfig().getLong("delay.time") * 20L));
 		} else {
