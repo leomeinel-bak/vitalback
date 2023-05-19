912729af81b6e75c50c7369d4d1328c9c504e29a
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-19 22:39:03 +0200

    Use non-deprecated getPluginMeta()

--- src/main/java/dev/meinel/leo/vitalback/VitalBack.java
+++ src/main/java/dev/meinel/leo/vitalback/VitalBack.java
@@ -2,7 +2,7 @@
  * File: VitalBack.java
  * Author: Leopold Meinel (leo@meinel.dev)
  * -----
- * Copyright (c) 2022 Leopold Meinel & contributors
+ * Copyright (c) 2023 Leopold Meinel & contributors
  * SPDX ID: GPL-3.0-or-later
  * URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * -----
@@ -37,8 +37,7 @@ public final class VitalBack
         setupStorage();
         messages = new Messages();
         Bukkit.getLogger()
-                .info("VitalBack v" + this.getDescription()
-                        .getVersion() + " enabled");
+                .info("VitalBack v" + this.getPluginMeta().getVersion() + " enabled");
         Bukkit.getLogger()
                 .info("Copyright (C) 2022 Leopold Meinel");
         Bukkit.getLogger()
@@ -52,8 +51,7 @@ public final class VitalBack
     @Override
     public void onDisable() {
         Bukkit.getLogger()
-                .info("VitalBack v" + this.getDescription()
-                        .getVersion() + " disabled");
+                .info("VitalBack v" + this.getPluginMeta().getVersion() + " disabled");
     }
 
     private void setupStorage() {
